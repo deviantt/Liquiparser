7b34f14bc370f6644d71b9f6be653cab8c4fe49f
Ilya <nesterovsusu@gmail.com>
2022-11-04 14:11:01 +0500

    init .py

---
+++ bot.py
@@ -0,0 +1,127 @@
+import telebot
+from telebot import types
+import parse
+import os
+
+
+bot = telebot.TeleBot(os.environ['LIQUIPARSER_TOKEN'])
+hide_board = types.ReplyKeyboardRemove()
+eventPool = ["The International 2022", "Shanghai Major"]
+choosed = dict()
+dota_event = None
+
+#Commands handlers
+@bot.message_handler(commands=["start"])
+def start(m):
+    bot.send_message(m.chat.id, "Hello! I will help you to get parsed matches for a tournament from Liquipedia")
+    handle_event_choice(m)
+
+@bot.message_handler(commands=["viewEvent"])
+def view_event(m):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    if choosed:
+        for i in choosed:
+            item = types.KeyboardButton(i)
+            markup.add(item)
+        message = bot.send_message(m.chat.id, "Which event do you want to view?", reply_markup=markup)
+        bot.register_next_step_handler(message, handle_team_view)
+    else:
+        bot.send_message(m.chat.id, "There are no configured event patterns")
+
+@bot.message_handler(commands=["reset"])
+def reset_event(m):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    for i in choosed:
+        item = types.KeyboardButton(i)
+        markup.add(item)
+    message = bot.send_message(m.chat.id, 'Choose an event to reset find pattern', reply_markup=markup)
+    bot.register_next_step_handler(message, handle_team_reset)
+
+@bot.message_handler(commands=["showMatches"])
+def show_matches(m):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    if choosed:
+        for i in choosed:
+            item = types.KeyboardButton(i)
+            markup.add(item)
+        message = bot.send_message(m.chat.id, "For which event do you want to show matches based on your pattern?", reply_markup=markup)
+        bot.register_next_step_handler(message, handle_event_matches)
+#Commands handlers
+
+def handle_event_matches(m):
+    choosed_teams = choosed[m.text]
+    print(choosed_teams)
+    global dota_event
+    if dota_event is not None:
+        if isinstance(dota_event, parse.DotaEvent):
+            match_list = dota_event.get_filtered_matches(choosed_teams)
+    print(match_list)
+    temp_str = ''
+    for match in match_list:
+        temp_str += f'{match}\n'
+    bot.send_message(m.chat.id, temp_str)
+
+def handle_team_reset(m):
+    choosed.pop(m.text)
+    bot.send_message(m.chat.id, f"{m.text} was successfully removed from your choosed events")
+
+def handle_team_view(m):
+    if m.text in choosed:
+        temp_str = ', '.join(i for i in choosed[m.text])
+        bot.send_message(m.chat.id, temp_str)
+
+def handle_event_choice(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard=True)
+    for i in range(len(eventPool)):
+        item = types.KeyboardButton(eventPool[i])
+        markup.add(item)
+    message = bot.send_message(m.chat.id,"Please choose a tournament:",reply_markup=markup)
+    bot.register_next_step_handler(message, handle_event)
+
+def handle_event(m):
+    if m.text in eventPool:
+        bot.send_message(m.chat.id, f"You chose {m.text}. Please wait a few seconds")
+        global dota_event
+        dota_event = parse.DotaEvent(parse.URL_OVERVIEW, [parse.URL_GROUPSTAGE_GAMES_1, parse.URL_GROUPSTAGE_GAMES_2], parse.URL_PLAYOFF)
+        choosed[dota_event.get_event_name()] = list()
+        handle_team(m, dota_event)
+    else:
+        bot.send_message(m.chat.id, "There is no such event")
+
+def handle_team(m, event):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    team_list = [item for item in event.get_participants() if item not in choosed[dota_event.get_event_name()]]
+    for i in range(len(team_list)):
+        item = types.KeyboardButton(team_list[i])
+        markup.add(item)
+    message = bot.send_message(m.chat.id, f"Choose your favourite team now:", reply_markup=markup)
+    bot.register_next_step_handler(message, handle_team_choice, event)
+
+def handle_team_choice(m, event):
+    if m.text in event.get_participants():
+        choosed[dota_event.get_event_name()].append(m.text)
+        bot.send_message(m.chat.id, "Added!")
+    else:
+        bot.send_message(m.chat.id, "Error!")
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard = True)
+    item_yes = types.KeyboardButton("Yes")
+    item_no = types.KeyboardButton("No")
+    markup.add(item_yes)
+    markup.add(item_no)
+    message = bot.send_message(m.chat.id, "Do you want to add another team?", reply_markup=markup)
+    bot.register_next_step_handler(message, handle_choice_next, event)
+
+def handle_choice_next(m, event):
+    if m.text == "Yes":
+        handle_team(m, event)
+    else:
+        bot.send_message(m.chat.id, "/showMatches")
+
+    
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+# TODO admin account + method for adding an event
+# TODO save knownusers?
+# TODO database?
+# TODO store chosen patterns?
---
+++ parse.py
@@ -0,0 +1,155 @@
+from bs4 import BeautifulSoup as bs
+import re
+import asyncio
+from aiohttp import ClientSession
+
+
+TIMEZONE_OFFSET = 3
+URL_GROUPSTAGE_GAMES_1 = "https://liquipedia.net/dota2/The_International/2022/Group_Stage_Day_1-2"
+URL_GROUPSTAGE_GAMES_2 = "https://liquipedia.net/dota2/The_International/2022/Group_Stage_Day_3-4"
+URL_OVERVIEW = "https://liquipedia.net/dota2/The_International/2022"
+URL_PLAYOFF = "https://liquipedia.net/dota2/The_International/2022/Main_Event"
+
+session = {}
+
+class DotaEvent:
+    def __init__(self, overview_url, groupstage_urls, playoff_url):
+        self.__overview_url = overview_url
+        self.__groupstage_urls = groupstage_urls
+        self.__playoff_url = playoff_url
+        self.__event_name = None
+        self.__participants = None
+        asyncio.run(self.__async_init())
+        
+    #public
+    def get_event_name(self):
+        return self.__event_name
+
+    def get_participants(self):
+        return self.__participants
+
+    def get_playoff_matches(self):
+        return self.__playoff_matches
+
+    def get_groupstage_matches(self):
+        return self.__groupstage_matches
+
+    def get_filtered_matches(self, team_names):
+        return self.__filter_matches(team_names)
+
+    #private
+    async def __async_init(self):
+        session['session'] = ClientSession()
+        async with session['session']:
+            overview_task = asyncio.create_task(self.__parse_overview())
+            groupstage_task = asyncio.create_task(self.__parse_groupstage_matches())
+            playoff_task = asyncio.create_task(self.__parse_playoff_matches())
+            await overview_task
+            await groupstage_task
+            await playoff_task
+
+    def __filter_matches(self, team_names):
+        out_list = list()
+        for team in team_names:
+            my_regex = r"(?<!\S)" + re.escape(team) + r"(?!\S)"
+            for groupstage_matches in self.__groupstage_matches:
+                for match in self.__groupstage_matches[groupstage_matches]:
+                    if re.search(my_regex, match) and match not in out_list:
+                        out_list.append(match)
+        return out_list
+
+    async def __parse_overview(self):
+        async with session['session'].get(self.__overview_url) as resp:
+            r = await resp.text()
+        soup = bs(r, "html.parser")
+        out_list = list()
+        participants = soup.find_all('div', class_='teamcard toggle-area toggle-area-1')
+        for team in participants:
+            out_list.append(team.a['title'])
+        self.__participants = out_list
+        event_name = soup.find('h1', class_='firstHeading')
+        self.__event_name = event_name.text
+    
+    async def __parse_playoff_matches(self):
+        async with session['session'].get(self.__playoff_url) as resp:
+            r = await resp.text()
+        soup = bs(r, "html.parser")
+        matches_raw = soup.find_all('tr', class_='Match')
+        out_list = list()
+        for i in range(len(matches_raw)):
+            temp_str: str
+            if matches_raw[i].find('span', class_='team-template-team2-short') is not None:
+                temp_str = str(matches_raw[i].find('span', class_='team-template-team2-short').a['title'] + " vs ")
+            else:
+                temp_str = "TBD vs "
+            if matches_raw[i].find('span', class_='team-template-team-short') is not None:
+                temp_str += str(matches_raw[i].find('span', class_='team-template-team-short').a['title'] + ".")
+            else:
+                temp_str += "TBD. "
+            temp_str += str("Round: " + matches_raw[i].find('td', class_='Round').text + ". ")
+            temp_str += str("Start time: " + self.__timezoneHandler(matches_raw[i].find('span', class_='timer-object timer-object-datetime-only').text, 5)[:-4])
+            out_list.append(temp_str)
+        self.__playoff_matches = out_list
+
+    async def __parse_groupstage_matches(self):
+        group_matches = dict()
+        tasks = []
+        for i in self.__groupstage_urls:
+            tasks.append(asyncio.create_task(self.__parse_groupstage_day(i)))
+        results = await asyncio.gather(*tasks)
+        for result in results:
+            group_matches = group_matches | result
+        self.__groupstage_matches = group_matches
+
+    async def __parse_groupstage_day(self, url):
+        
+        async with session['session'].get(url) as resp:
+            r = await resp.text()
+        soup = bs(r, "html.parser")
+        game_days = list()
+        days_tables = soup.find_all('div', class_='brkts-matchlist brkts-matchlist-collapsible')
+        for i in range(len(days_tables)):
+            game_days.append(days_tables[i].find('div', class_='brkts-matchlist-title').text)
+        out_dict = dict()
+        for i in range(len(game_days)):
+            out_dict[game_days[i]] = self.__parse_groupstage_gamesperday(days_tables[i])
+        return out_dict
+
+    def __parse_groupstage_gamesperday(self, days_tables):
+        team_names = days_tables.find_all('span', class_='name')
+        start_times = days_tables.find_all('span', class_='timer-object')
+        out_list = list()
+        for i in range(len(team_names)):
+            if team_names[i].a is not None:
+                temp_str: str
+                if i % 2 == 0:
+                    temp_str = team_names[i].a['title'] + ' vs '
+                else:
+                    temp_str += f"{team_names[i].a['title']} | Start time: {self.__timezoneHandler(start_times[i//4].text, -3)[:-4]}"
+                    out_list.append(temp_str)
+        return out_list
+
+    def __timezoneHandler(self, datetime, timezone_offset):
+        new_time = int(re.search('[0-9]{2}:', datetime).group(0)[:-1]) + timezone_offset
+        time_index = datetime.find(re.search('[0-9]{2}:', datetime).group(0))
+        if new_time < 10:
+            new_time = "0" + str(new_time)
+        else:
+            new_time = str(new_time)
+        datetime = datetime[:time_index] + new_time + datetime[time_index + 2:]
+        return datetime
+    
+
+def print_dict(dictionary): #console app testing
+    for key in dictionary:
+        print(key + ':')
+        for i in range(len(dictionary[key])):
+            print(str(i + 1) + ". " + dictionary[key][i])
+
+def print_list(list): #console app testing
+    for i in range(len(list)):
+        print(f"{i+1}. {list[i]}")
+
+    
+if __name__=="__main__":
+    pass
